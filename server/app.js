312c937d9b8ee13df022e695126ac15ca783a33c
KotaAoyama <hongcha811@gmail.com>
2020-02-15 00:16:53 +0900

    multiple search

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require("express");
 const path = require("path");
 const { ES_INDEX } = require("../seeds/seeds");
@@ -13,23 +15,43 @@ const setupServer = () => {
   app.get("/api/search", async (req, res) => {
 
     const query = req.query.recipe_search;
-    const { body } = await client.search({
-      index: ES_INDEX,
-      body: {
-        query: {
-          match: {
-            title: query
-          },
-        },
-        query: {
-          match: {
-            ingredients: query
+
+    const { body } = await client.msearch({
+      body: [
+        { index: ES_INDEX },
+        { query: { match: { title: query } } },
+  
+        { index: ES_INDEX },
+        { query: { match: { description: query } } },
+
+        { index: ES_INDEX },
+        { query: { match: { ingredients: query } } },
+
+        { index: ES_INDEX },
+        { query: { match: { directions: query } } },
+
+        { index: ES_INDEX },
+        { query: { match: { tags: query } } },
+
+        { index: ES_INDEX },
+        { query: { match: { author: query } } },
+      ]
+    }).catch(console.log);
+
+    const result = [];
+    for (const responsesPerSearch of body.responses) {
+
+      if (responsesPerSearch.hits.hits.length > 0) {
+        for (const resPerSearch of responsesPerSearch.hits.hits) {
+
+          if (!result.includes(resPerSearch._id)) {
+            result.push(resPerSearch._id);
           }
         }
       }
-    }).catch(console.log);
-
-    console.log(body.hits.hits);
+    }
+    console.log('result', result, result.length);
+    res.send(result);
   });
 
   return app;
