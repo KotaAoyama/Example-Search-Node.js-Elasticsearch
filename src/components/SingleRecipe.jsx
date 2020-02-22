4b074c64b29c3f0d98f26c6af3391d72948a0ad8
KotaAoyama <hongcha811@gmail.com>
2020-02-22 23:58:45 +0900

    show table

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/SingleRecipe.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeSelectedRecipe } from '../redux/index';
 
@@ -24,7 +25,56 @@ export default function SingleRecipe() {
       </h4>
 
       <div id="showDocument">
-        {JSON.stringify(selectedRecipe)}
+        <table>
+          <thead>
+            <tr>
+              <td>Key</td>
+              <td>Value</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>title</td>
+              <td>{selectedRecipe.title}</td>
+            </tr>
+            <tr>
+              <td>description</td>
+              <td>{selectedRecipe.description}</td>
+            </tr>
+            <tr>
+              <td>ingredients</td>
+              <td>{selectedRecipe.ingredients}</td>
+            </tr>
+            <tr>
+              <td>directions</td>
+              <td>{selectedRecipe.directions}</td>
+            </tr>
+            <tr>
+              <td>prep_time_min</td>
+              <td>{selectedRecipe.prep_time_min}</td>
+            </tr>
+            <tr>
+              <td>cook_time_min</td>
+              <td>{selectedRecipe.cook_time_min}</td>
+            </tr>
+            <tr>
+              <td>servings</td>
+              <td>{selectedRecipe.servings}</td>
+            </tr>
+            <tr>
+              <td>tags</td>
+              <td>{selectedRecipe.tags}</td>
+            </tr>
+            <tr>
+              <td>author</td>
+              <td>{selectedRecipe.author.name}</td>
+            </tr>
+            <tr>
+              <td>source_url</td>
+              <td>{selectedRecipe.source_url}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   )
