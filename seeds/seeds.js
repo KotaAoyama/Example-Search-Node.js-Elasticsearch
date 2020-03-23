196701f52467bacc0ce5142a384b03a3c9de1097
Kota Aoyama <hongcha811@gmail.com>
2020-03-23 17:09:52 +0900

    deleted unnecessary comments and logs

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -19,7 +19,6 @@ async function run () {
     const { body } = await client.indices.delete({
       index: ES_INDEX
     })
-    console.log('deleted', body);
   }
 
   const dataset = [];
@@ -44,16 +43,12 @@ async function run () {
 
       if (bulkResponse.errors) {
         const erroredDocuments = []
-        // The items array has the same order of the dataset we just indexed.
-        // The presence of the `error` key indicates that the operation
-        // that we did for the document has failed.
+        
         bulkResponse.items.forEach((action, i) => {
           const operation = Object.keys(action)[0]
+          
           if (action[operation].error) {
             erroredDocuments.push({
-              // If the status is 429 it means that you can retry the document,
-              // otherwise it's very likely a mapping error, and you should
-              // fix the document before to try it again.
               status: action[operation].status,
               error: action[operation].error,
               operation: body[i * 2],
@@ -64,7 +59,6 @@ async function run () {
         console.log(erroredDocuments)
       }
       const { body: count } = await client.count({ index: ES_INDEX })
-      console.log(count)
     }
     bulk();
   });
